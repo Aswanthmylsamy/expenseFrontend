fdbfe6fa684324e66a5d799e89512266491034d0
Gokul N A <gokulna@Gokuls-MacBook-Air.local>
2025-06-26 15:53:51 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Print from './index.jsx'
 import react from 'react'
-import './index.jsx'
-import Form from './form.jsx';
 import History from './History.jsx'
 import './ExpenseForm.jsx'
 import ExpenseForm from './ExpenseForm.jsx'
 import ExpenseContainer from './ExpenseContainer.jsx'
-import Newc from './Newc.jsx'
 
 
 function App() {
